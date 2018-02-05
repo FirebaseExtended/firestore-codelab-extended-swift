ca8cf6fa4c993e91edd46037ceeaea255601cb2a
Jen Person <personjeh@gmail.com>
2018-02-05 14:55:13 -0800

    Saving action now works and includes alert. Still need to incorporate image saving, if desired

--- FriendlyEats/EditRestaurantViewController.swift
+++ FriendlyEats/EditRestaurantViewController.swift
@@ -12,7 +12,9 @@ import Firebase
 class EditRestaurantViewController: UIViewController {
 
     // MARK: Properties
+    
     var restaurant: Restaurant?
+    var imagePicker = UIImagePickerController()
     
     // MARK: Outlets
     
@@ -34,6 +36,7 @@ class EditRestaurantViewController: UIViewController {
         }
     }
 
+    // populate restaurant with current data
     func populateRestaurant() {
         restaurantNameTextField.text = restaurant?.name
         locationTextField.text = restaurant?.city
@@ -41,8 +44,72 @@ class EditRestaurantViewController: UIViewController {
         priceTextField.text = restaurant?.price.description
         restaurantImageView.sd_setImage(with: restaurant?.photoURL)
     }
+    
+    func saveChanges() {
+        let data = [
+            "name": restaurantNameTextField.text,
+            "city": locationTextField.text,
+            "category": cuisineTextField.text,
+            "price": Int(priceTextField.text!)
+            //"photoURL": dostuffhere
+            ] as [String : Any]
+        Firestore.firestore().collection("restaurants").document((restaurant?.documentID)!).updateData(data){ err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                self.handleAlert(isSaving: false)
+            }
+        }
+    }
 
+    func handleAlert(isSaving: Bool) {
+        var message = "Are you sure you want to save changes to this restaurant?"
+        if !isSaving {
+            message = "Successfully saved!"
+        }
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            self.saveChanges()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let okAction = UIAlertAction(title: "OK", style: .default) { action in
+            self.navigationController?.popViewController(animated: true)
+        }
+        if isSaving {
+            alertController.addAction(saveAction)
+            alertController.addAction(cancelAction)
+        } else {
+            alertController.addAction(okAction)
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func selectNewImage(_ sender: Any) {
-        
+        selectImage()
+    }
+    
+    @IBAction func didSelectSaveChanges(_ sender: Any) {
+        handleAlert(isSaving: true)
+    }
+    
+}
+
+extension EditRestaurantViewController: UIImagePickerControllerDelegate {
+    
+    func selectImage() {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            
+            imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            imagePicker.sourceType = .savedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismiss(animated: true, completion: { () -> Void in
+            // save image
+        })
     }
 }
