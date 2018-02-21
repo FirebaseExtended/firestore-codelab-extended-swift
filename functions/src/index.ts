d5efea80b6e7671ff277e67d1fd32ed3d927c47a
Jen Person <personjeh@gmail.com>
2018-02-21 12:54:35 -0800

    updated functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,13 +5,22 @@ exports.computeAverageReview = functions.firestore
   .document('reviews/{reviewId}').onWrite((event) => {
     // get the data from the write event
     const eventData = event.data.data();
-    const restaurantID = eventData.restaurantID;
-    const rating = eventData.rating;
-    const db = event.data.ref.firestore;
-    
     // get the previous value, if it exists
     const prev = event.data.previous;
-    const previousValue = event.data.previous.data();
+    const previousValue = prev.data();
+    const rating = eventData.rating;
+    const prevRating = previousValue.rating;
+    if (rating === prevRating) {
+        console.log("not a new rating.");
+        return "Not a new rating";
+    }
+    // get the restaurant ID
+    const restaurantID = eventData.restaurantID;
+    // get a reference to the root of the firestore DB
+    const db = event.data.ref.firestore;
+    
+    // if a previous value exists, then it needs to be replaced
+    // when computing an average. Otherwise, add the new rating
     if (prev.exists) {
         const difference = previousValue.rating - rating
         return updateAverage(db, restaurantID, difference, true);
@@ -19,6 +28,21 @@ exports.computeAverageReview = functions.firestore
         return updateAverage(db, restaurantID, rating, false);
     }
   });
+
+exports.updateRest = functions.firestore.document('restaurants/{restaurantID}').onUpdate((event) => {
+    const db = event.data.ref.firestore;
+    const restaurantID = event.params.restaurantID;
+    const eventData = event.data.data();
+    const prevEventData = event.data.previous.data();
+    const name = eventData.name;
+    const oldName = prevEventData.name;
+    if (oldName === name) {
+        console.log("change was not in name. No need to update reviews.");
+        return "change was not in name. No need to update reviews.";
+    }
+    // if one or more of these fields was updated
+    return updateRestaurant(db, restaurantID, name);
+});
   
   async function updateAverage(db, restaurantID, newRating, prev) {
     const updateDB = db.collection('restaurants').doc(restaurantID);
@@ -45,13 +69,7 @@ exports.computeAverageReview = functions.firestore
    return transactionResult;
   }
 
-exports.updateRestaurant = functions.firestore.document('restaurants/{restaurantID}/name/').onUpdate((event) => {
-    const db = event.data.ref.firestore;
-    const restaurantID = event.params.restaurantID;
-    const eventData = event.data.data();
-    // if one or more of these fields was updated
-    return updateRestaurant(db, restaurantID, eventData);
-})
+
 
 // update changes to restaurant
 async function updateRestaurant(db: Firestore, restaurantID, data) {
@@ -64,9 +82,12 @@ async function updateRestaurant(db: Firestore, restaurantID, data) {
         return (async () => {
             const reviewsSnapshot = await t.get(queryRef);
             for (const doc of reviewsSnapshot.docs) {
-                promises.push(await db.doc(doc.ref.path).update({name: data}));
+                console.log(doc);
+                console.log(doc.ref.path)
+                const result = await t.update(doc.ref, {name: data});
+                promises.push(result);
             };
-            return Promise.all(promises);
+            return promises;
         })();
     }) 
     return transactionResult; 
